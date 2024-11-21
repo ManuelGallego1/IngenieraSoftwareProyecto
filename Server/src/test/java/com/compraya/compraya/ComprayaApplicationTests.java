e7a9216eee3a2dc66516c89dee3801cbda6a6e08
Samuel05L28 <samuelescobaralvarez@gmail.com>
2024-11-20 20:10:51 -0500

    Template

---
+++ Server/src/test/java/com/compraya/compraya/ComprayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compraya.compraya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
